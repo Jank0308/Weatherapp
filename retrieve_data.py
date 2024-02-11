1adad43cbdffae38d7afe36a6844ada2dea56023
Jan Kursawe <118570892+Jank0308@users.noreply.github.com>
2024-02-11 12:10:10 +0100

    Add files via upload

---
+++ retrieve_data.py
@@ -0,0 +1,43 @@
+def ret_data(city):
+
+    import requests, json
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    api_key = "8eec4a33213199cae5d7a83f10410a0c"
+
+    
+    
+
+    if True:
+        city_name = city
+        complete_url = base_url+"appid="+api_key+"&q="+city_name
+
+        response = requests.get(complete_url)
+        x = response.json()
+
+    if x["cod"] == "404":
+       data = 'Sorry,'+ city + 'not found\n Try things like bracets or similar'
+       return data
+       pass
+
+    if x["cod"] != "404":
+        y = x["main"]
+        temp = y["temp"]
+        press = y["pressure"]
+        humid = y["humidity"]
+
+        z = x["weather"]
+        descrip = z[0]["description"]
+        
+        data = ("current Temp in C° = " +
+              str(temp-273.15) +
+              "\n\n pressure in hPa = " + 
+              str(press) + 
+              "\n\n humidity in % = " +
+              str(humid) +
+              "\n\n Weather = " + 
+              str(descrip)          
+              )
+
+
+    
+    return data
